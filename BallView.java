0cb42f7619373aecd823a619086ac71e3a72d140
AliyA <s_c00b_y@hotmail.com>
2011-07-07 14:05:15 -0700

    Kommentare übersetzt

--- BallView.java
+++ BallView.java
@@ -3,26 +3,25 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 
 /**
- * Write a description of class BallView here.
+ * Die Klasse Ballview. In dieser Klasse wird das Objekt Ball definiert und erstellt.
  * 
- * @author (your name)
- * @version (a version number or a date)
+ * @Michael
  */
 public class BallView {
 	private Ball ball;
 
 	/**
-	 * Constructor for objects of class BallView
+	 * Konstruktor des Balles
 	 */
 	public BallView(Ball ball) {
 		this.ball = ball;
 	}
 
 	/**
-	 * Renders the ball onto the screen.
+	 * Zeigt den Ball auf dem Spielfeld
 	 * 
 	 * @param g
-	 *            the graphics context in which to draw.
+	 *            Der grafische Index in der sich das Objekt befindet.
 	 */
 	public void paint(Graphics g) {
 		g.setColor(Color.WHITE);
