4465315016d01670a11b4e69b5a6da79b8dbf551
AliyA <s_c00b_y@hotmail.com>
2011-07-08 01:29:52 -0700

    Edited unitTest/GameTest.java via GitHub

--- unitTest/GameTest.java
+++ unitTest/GameTest.java
@@ -13,13 +13,21 @@ import org.junit.Test;
  */
 public class GameTest {
 
+/**
+ *  Dieser Test testet, ob die Grösse des Fensters richtig gesetzt ist.
+ *
+ */
+
 public void testGameBounds()
     {
         Game game1 = new Game();
         assertEquals(new Rectangle(0,0,500,500), game1.getBounds());
     }
 
-
+/**
+ *  Dieser Test testet, ob das Level am Anfang des SPiels richtig gesetzt wird
+ *
+ */
 
 public void testLevelAtGameStart()
     {
@@ -27,6 +35,10 @@ public void testLevelAtGameStart()
         assertEquals(1, game1.getLevel());
     }
 
+/**
+ *  Dieser Test testet, ob man korrekt Level selber setzten kann.
+ *
+ */
 
 public void testSetLevel()
     {
@@ -34,6 +46,13 @@ public void testSetLevel()
         game1.setLevel(5);
         assertEquals(5, game1.getLevel());
     }
+
+/**
+ *  Dieser Test testet die Methoden setMoveplayer und Tick. Zuerst wird der Spieler mit setMoveplayer bewegt, dann führt man 
+ * einen Tick aus, wo dann die Änderungen gemacht werden. Schlussendlich wird geprüft, ob tick die Änderungen richtig 
+ * ausgeführt hat
+ *
+ */
 
 public void testSetMoveplayerOneAndTick()
     {
@@ -43,6 +62,12 @@ public void testSetMoveplayerOneAndTick()
         assertEquals(new Rectangle(480,231,10,40), game1.getPlayer().getArea());
     }
     
+
+/**
+ *  gleicher Test wie oben, einfach in umgekehrte Richtung
+ *
+ */
+
 public void testSetMoveplayerMinusOneAndTick()
     {
         Game game1 = new Game();
@@ -51,11 +76,21 @@ public void testSetMoveplayerMinusOneAndTick()
         assertEquals(new Rectangle(480,229,10,40), game1.getPlayer().getArea());
     }
 
+/**
+ *  In diesem Test wird die Ball Position am Start des SPiels überprüft.
+ *
+ */
+
 public void testBallPosition()
     {
         Game game1 = new Game();
         assertEquals(new Rectangle(30,245,10,10), game1.getBall().getArea());
     }
+
+/**
+ *  In diesem Test wird die Paddle Position am Start des Spiels überprüft.
+ *
+ */
 
 public void testPaddlePlayerPosition()
     {
@@ -63,6 +98,12 @@ public void testPaddlePlayerPosition()
         assertEquals(new Rectangle(480,230,10,40), game1.getPlayer().getArea());
     }
     
+
+/**
+ *  In diesen 5 Tests wwird die Methode isWithinBounds geprüft. In der Methode sind 5 Rückgabewerte möglich. Jede dieser 
+ *  Möglichkeiten wird überprüft.
+ *
+ */
 public void testIsWithinBounds1()
     {
         Game game1 = new Game();
