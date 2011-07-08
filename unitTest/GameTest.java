f2086f28ad8379435df1d7b711fcd1a935dc4f7d
AliyA <s_c00b_y@hotmail.com>
2011-07-08 01:09:53 -0700

    Edited unitTest/GameTest.java via GitHub

--- unitTest/GameTest.java
+++ unitTest/GameTest.java
@@ -13,140 +13,96 @@ import org.junit.Test;
  */
 public class GameTest {
 
-	/**
-	 * Test method for {@link Game#Game()}.
-	 */
-	@Test
-	public final void testGame() {
-		fail("Not yet implemented"); // TODO
-	}
+public void testGameBounds()
+    {
+        Game game1 = new Game();
+        assertEquals(new Rectangle(0,0,500,500), game1.getBounds());
+    }
 
-	/**
-	 * Test method for {@link Game#getBall()}.
-	 */
-	@Test
-	public final void testGetBall() {
-		fail("Not yet implemented"); // TODO
-	}
 
-	/**
-	 * Test method for {@link Game#getBounds()}.
-	 */
-	@Test
-	public final void testGetBounds() {
-		fail("Not yet implemented"); // TODO
-	}
 
-	/**
-	 * Test method for {@link Game#getComp()}.
-	 */
-	@Test
-	public final void testGetComp() {
-		fail("Not yet implemented"); // TODO
-	}
+public void testLevelAtGameStart()
+    {
+        Game game1 = new Game();
+        assertEquals(1, game1.getLevel());
+    }
 
-	/**
-	 * Test method for {@link Game#getGameState()}.
-	 */
-	@Test
-	public final void testGetGameState() {
-		fail("Not yet implemented"); // TODO
-	}
 
-	/**
-	 * Test method for {@link Game#getLevel()}.
-	 */
-	@Test
-	public final void testGetLevel() {
-		fail("Not yet implemented"); // TODO
-	}
+public void testSetLevel()
+    {
+        Game game1 = new Game();
+        game1.setLevel(5);
+        assertEquals(5, game1.getLevel());
+    }
 
-	/**
-	 * Test method for {@link Game#getPlayer()}.
-	 */
-	@Test
-	public final void testGetPlayer() {
-		fail("Not yet implemented"); // TODO
-	}
+public void testSetMoveplayerOneAndTick()
+    {
+        Game game1 = new Game();
+        game1.setMoveplayer(1);
+        game1.tick();
+        assertEquals(new Rectangle(480,231,10,40), game1.getPlayer().getArea());
+    }
+    
+public void testSetMoveplayerMinusOneAndTick()
+    {
+        Game game1 = new Game();
+        game1.setMoveplayer(-1);
+        game1.tick();
+        assertEquals(new Rectangle(480,229,10,40), game1.getPlayer().getArea());
+    }
 
-	/**
-	 * Test method for {@link Game#hitsPaddle(java.awt.Rectangle)}.
-	 */
-	@Test
-	public final void testHitsPaddle() {
-		fail("Not yet implemented"); // TODO
-	}
+public void testBallPosition()
+    {
+        Game game1 = new Game();
+        assertEquals(new Rectangle(30,245,10,10), game1.getBall().getArea());
+    }
 
-	/**
-	 * Test method for {@link Game#idle()}.
-	 */
-	@Test
-	public final void testIdle() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link Game#isWithinBounds(java.awt.Rectangle)}.
-	 */
-	@Test
-	public final void testIsWithinBounds() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link Game#lose()}.
-	 */
-	@Test
-	public final void testLose() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link Game#newlevel()}.
-	 */
-	@Test
-	public final void testNewlevel() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link Game#setLevel(int)}.
-	 */
-	@Test
-	public final void testSetLevel() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link Game#setMoveplayer(int)}.
-	 */
-	@Test
-	public final void testSetMoveplayer() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link Game#start()}.
-	 */
-	@Test
-	public final void testStart() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link Game#tick()}.
-	 */
-	@Test
-	public final void testTick() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	/**
-	 * Test method for {@link Game#win()}.
-	 */
-	@Test
-	public final void testWin() {
-		fail("Not yet implemented"); // TODO
-	}
+public void testPaddlePlayerPosition()
+    {
+        Game game1 = new Game();
+        assertEquals(new Rectangle(480,230,10,40), game1.getPlayer().getArea());
+    }
+    
+public void testIsWithinBounds1()
+    {
+        Game game1 = new Game();
+        Paddle testpaddle = new Paddle(game1,0,0);
+        testpaddle.setPosition(-1,0);
+        assertEquals(1, game1.isWithinBounds(testpaddle.getArea()));
+        
+    }
+    
+public void testIsWithinBounds2()
+    {
+        Game game1 = new Game();
+        Paddle testpaddle = new Paddle(game1,0,0);
+        testpaddle.setPosition(0,-1);
+        assertEquals(2, game1.isWithinBounds(testpaddle.getArea()));
+        
+    }
+public void testIsWithinBounds3()
+    {
+        Game game1 = new Game();
+        Paddle testpaddle = new Paddle(game1,0,0);
+        testpaddle.setPosition(501,0);
+        assertEquals(3, game1.isWithinBounds(testpaddle.getArea()));
+        
+    }
+public void testIsWithinBounds4()
+    {
+        Game game1 = new Game();
+        Paddle testpaddle = new Paddle(game1,0,0);
+        testpaddle.setPosition(0,501);
+        assertEquals(4, game1.isWithinBounds(testpaddle.getArea()));
+        
+    }  
+public void testIsWithinBounds5()
+    {
+        Game game1 = new Game();
+        Paddle testpaddle = new Paddle(game1,0,0);
+        testpaddle.setPosition(232,400);
+        assertEquals(0, game1.isWithinBounds(testpaddle.getArea()));
+        
+    }   
 
 }
